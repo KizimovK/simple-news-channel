d66f4673c454764af0a80d53a05bd828c83e94b9
Константин Кизимов <kizim_net@mail.ru>
2024-01-31 22:37:00 +0400

    Start project

---
+++ src/main/java/com/example/simplenewschannel/SimpleNewsChannelApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplenewschannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNewsChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleNewsChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplenewschannel/SimpleNewsChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplenewschannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNewsChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
